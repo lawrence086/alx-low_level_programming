75a1951a4cefdbe84bdbcf855f25522961facb2a
Lawrence Sekgoka <lawrencesekgoka086@gmail.com>
2022-12-08 14:44:36 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main – It serves as the entry point for the program
+ * Return: It returns zero by default if no errors otherwise it returns non-zero value.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
